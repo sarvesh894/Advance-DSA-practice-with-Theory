363af476f90c6fc34d56fa0c6ad65235c71b014e
sarvesh894 <sarveshmishra7617@gmail.com>
2025-09-22 11:45:15 +0530

    Initial commit

---
+++ FirstHighest.java
@@ -0,0 +1,14 @@
+public class FirstHighest {
+    public static void main(String[] args) {
+        int[] arr = {12, 45, 67, 23, 89, 34};
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("First highest element: " + max);
+    }
+}
