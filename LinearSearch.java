0b36cb38871159c64a717f9bae6520ca6ceb73fe
sarvesh894 <sarveshmishra7617@gmail.com>
2025-09-22 15:17:30 +0530

    Initial commit

---
+++ LinearSearch.java
@@ -0,0 +1,19 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {10, 25, 30, 45, 50, 65};
+        int target = 45;
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                System.out.println("Element found at index: " + i);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Element not found in the array.");
+        }
+    }
+}
