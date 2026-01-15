9b04a36424e4dc83c7f9a57dfbecf20ff9b0086f
sarvesh894 <sarveshmishra7617@gmail.com>
2026-01-16 04:49:14 +0530

    insrt at end in linkedlist

---
+++ InsertAtEndLinkedList.java
@@ -0,0 +1,43 @@
+public class InsertAtEndLinkedList {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    public void traverse() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        InsertAtEndLinkedList list = new InsertAtEndLinkedList();
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+
+        list.traverse();
+    }
+}
