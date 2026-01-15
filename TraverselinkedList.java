95d824349a686805699e4c352da9a3896a20d776
sarvesh894 <sarveshmishra7617@gmail.com>
2026-01-15 13:47:43 +0530

    Add TraverseLinkedList program

---
+++ TraverselinkedList.java
@@ -0,0 +1,42 @@
+class Node {
+    int data;
+    Node next;
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class TraverseLinkedList {
+    Node head;
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    public void traverse() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        TraverseLinkedList list = new TraverseLinkedList();
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.insert(40);
+        list.traverse();
+    }
+}
