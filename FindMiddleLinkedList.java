01152d60d8d9c8fae623ee16f30cebc517e77905
sarvesh894 <sarveshmishra7617@gmail.com>
2026-01-16 04:56:23 +0530

    Find Middle element in ll

---
+++ FindMiddleLinkedList.java
@@ -0,0 +1,59 @@
+public class FindMiddleLinkedList {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    public void findMiddle() {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (slow != null) {
+            System.out.println("Middle element: " + slow.data);
+        }
+    }
+
+    public void traverse() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        FindMiddleLinkedList list = new FindMiddleLinkedList();
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+        list.insertAtEnd(40);
+        list.insertAtEnd(50);
+
+        list.traverse();
+        list.findMiddle();
+    }
+}
