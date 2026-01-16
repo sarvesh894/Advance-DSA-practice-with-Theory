b470f72b1c1ac1eaaacd5d5499a01d66d287b805
sarvesh894 <sarveshmishra7617@gmail.com>
2026-01-16 11:46:46 +0530

    message

---
+++ MergeTwoSortedLists.java
@@ -0,0 +1,71 @@
+public class MergeTwoSortedLists {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    public static Node merge(Node l1, Node l2) {
+        Node dummy = new Node(0);
+        Node tail = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.data <= l2.data) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+
+        if (l1 != null) tail.next = l1;
+        if (l2 != null) tail.next = l2;
+
+        return dummy.next;
+    }
+
+    public static void traverse(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        MergeTwoSortedLists list1 = new MergeTwoSortedLists();
+        list1.insertAtEnd(1);
+        list1.insertAtEnd(3);
+        list1.insertAtEnd(5);
+
+        MergeTwoSortedLists list2 = new MergeTwoSortedLists();
+        list2.insertAtEnd(2);
+        list2.insertAtEnd(4);
+        list2.insertAtEnd(6);
+
+        Node mergedHead = merge(list1.head, list2.head);
+        traverse(mergedHead);
+    }
+}
