b98be908ccc783a227e35d7bf99f0e11560b81cf
sarvesh894 <sarveshmishra7617@gmail.com>
2026-01-16 11:54:43 +0530

    message

---
+++ OddEvenLinkedList.java
@@ -0,0 +1,70 @@
+public class OddEvenLinkedList {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    public void oddEvenReorder() {
+        if (head == null || head.next == null) return;
+
+        Node odd = head;
+        Node even = head.next;
+        Node evenHead = even;
+
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+    }
+
+    public void traverse() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        OddEvenLinkedList list = new OddEvenLinkedList();
+        list.insertAtEnd(1);
+        list.insertAtEnd(2);
+        list.insertAtEnd(3);
+        list.insertAtEnd(4);
+        list.insertAtEnd(5);
+
+        System.out.println("Original List:");
+        list.traverse();
+
+        list.oddEvenReorder();
+
+        System.out.println("Odd-Even Reordered List:");
+        list.traverse();
+    }
+}
