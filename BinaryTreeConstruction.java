22cdda213794c6812dd8a88e93b677de114e907e
sarvesh894 <sarveshmishra7617@gmail.com>
2026-01-18 00:54:55 +0530

    message

---
+++ BinaryTreeConstruction.java
@@ -0,0 +1,77 @@
+public class BinaryTreeConstruction {
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    static class BinaryTree {
+        Node root;
+
+        BinaryTree() {
+            root = null;
+        }
+
+        public Node insert(Node root, int data) {
+            if (root == null) {
+                root = new Node(data);
+                return root;
+            }
+            if (data < root.data) {
+                root.left = insert(root.left, data);
+            } else {
+                root.right = insert(root.right, data);
+            }
+            return root;
+        }
+
+        public void inorder(Node root) {
+            if (root != null) {
+                inorder(root.left);
+                System.out.print(root.data + " ");
+                inorder(root.right);
+            }
+        }
+
+        public void preorder(Node root) {
+            if (root != null) {
+                System.out.print(root.data + " ");
+                preorder(root.left);
+                preorder(root.right);
+            }
+        }
+
+        public void postorder(Node root) {
+            if (root != null) {
+                postorder(root.left);
+                postorder(root.right);
+                System.out.print(root.data + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.root = tree.insert(tree.root, 50);
+        tree.insert(tree.root, 30);
+        tree.insert(tree.root, 70);
+        tree.insert(tree.root, 20);
+        tree.insert(tree.root, 40);
+        tree.insert(tree.root, 60);
+        tree.insert(tree.root, 80);
+
+        System.out.println("Inorder Traversal:");
+        tree.inorder(tree.root);
+
+        System.out.println("\nPreorder Traversal:");
+        tree.preorder(tree.root);
+
+        System.out.println("\nPostorder Traversal:");
+        tree.postorder(tree.root);
+    }
+}
